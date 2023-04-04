8327d91ef2cb6bdea5e28a8428d590c89a85e003
Jakub Łanoszka <jakublanoszka@gmail.com>
2023-04-04 18:13:42 +0200

    added invoice view with filtering

--- src/main/java/io/mkolodziejczyk92/views/invoice/InvoicesView.java
+++ src/main/java/io/mkolodziejczyk92/views/invoice/InvoicesView.java
@@ -1,216 +1,142 @@
 package io.mkolodziejczyk92.views.invoice;
 
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.checkbox.Checkbox;
+
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.dependency.Uses;
-import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.grid.HeaderRow;
+import com.vaadin.flow.component.grid.dataview.GridListDataView;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.notification.Notification.Position;
-import com.vaadin.flow.component.notification.NotificationVariant;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.splitlayout.SplitLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.BeanValidationBinder;
-import com.vaadin.flow.data.binder.ValidationException;
-import com.vaadin.flow.data.renderer.LitRenderer;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
 import io.mkolodziejczyk92.data.entity.Invoice;
 import io.mkolodziejczyk92.data.service.InvoiceService;
 import io.mkolodziejczyk92.views.MainLayout;
 import jakarta.annotation.security.PermitAll;
-import java.util.Optional;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.orm.ObjectOptimisticLockingFailureException;
 
-@PageTitle("Invoice")
+import java.util.List;
+import java.util.function.Consumer;
+
+import static io.mkolodziejczyk92.views.FilterHeader.getComponent;
+
+@PageTitle("Invoices")
 @Route(value = "invoices", layout = MainLayout.class)
 @PermitAll
 @Uses(Icon.class)
-public class InvoicesView extends Div implements BeforeEnterObserver {
-
-    private final String INVOICE_ID = "invoiceID";
-    private final String INVOICE_EDIT_ROUTE_TEMPLATE = "Invoice/%s/edit";
+public class InvoicesView extends Div {
 
     private final Grid<Invoice> grid = new Grid<>(Invoice.class, false);
-
-    private TextField number;
-    private TextField amount;
-    private TextField contractNumber;
-    private TextField client;
-    private TextField issueDate;
-    private TextField paymentTime;
-    private TextField type;
-    private Checkbox paid;
-    private TextField paymentMethod;
-
-    private final Button cancel = new Button("Cancel");
-    private final Button save = new Button("Save");
-
-    private final BeanValidationBinder<Invoice> binder;
-
-    private Invoice invoice;
 
     private final InvoiceService invoiceService;
 
     public InvoicesView(InvoiceService invoiceService) {
         this.invoiceService = invoiceService;
-        addClassNames("invoice-view");
 
-        // Create UI
-        SplitLayout splitLayout = new SplitLayout();
+        Grid.Column<Invoice> invoiceNumberColumn =
+                grid.addColumn(Invoice::getNumber).setAutoWidth(true);
 
-        createGridLayout(splitLayout);
-        createEditorLayout(splitLayout);
+        Grid.Column<Invoice> clientFullNameColumn =
+                grid.addColumn(invoice -> invoice.getClient().getFullName()).setAutoWidth(true);
 
-        add(splitLayout);
+        Grid.Column<Invoice> contractNumberColumn =
+                grid.addColumn(invoice -> invoice.getContract().getNumber()).setAutoWidth(true);
 
-        // Configure Grid
-        grid.addColumn("number").setAutoWidth(true);
-        grid.addColumn("amount").setAutoWidth(true);
-        grid.addColumn("contractNumber").setAutoWidth(true);
-        grid.addColumn("client").setAutoWidth(true);
-        grid.addColumn("issueDate").setAutoWidth(true);
-        grid.addColumn("paymentTime").setAutoWidth(true);
-        grid.addColumn("type").setAutoWidth(true);
-        LitRenderer<Invoice> paidRenderer = LitRenderer.<Invoice>of(
-                "<vaadin-icon icon='vaadin:${item.icon}' style='width: var(--lumo-icon-size-s); height: var(--lumo-icon-size-s); color: ${item.color};'></vaadin-icon>")
-                .withProperty("icon", paid -> paid.isPaid() ? "check" : "minus").withProperty("color",
-                        paid -> paid.isPaid() ? "var(--lumo-primary-text-color)" : "var(--lumo-disabled-text-color)");
 
-        grid.addColumn(paidRenderer).setHeader("Paid").setAutoWidth(true);
+        Grid.Column<Invoice> amountColumn =
+                grid.addColumn(Invoice::getAmount).setAutoWidth(true);
 
-        grid.addColumn("paymentMethod").setAutoWidth(true);
-        grid.setItems(query -> invoiceService.list(
-                PageRequest.of(query.getPage(), query.getPageSize(), VaadinSpringDataHelpers.toSpringDataSort(query)))
-                .stream());
+        Grid.Column<Invoice> issueDateColumn =
+                grid.addColumn(Invoice::getIssueDate).setAutoWidth(true);
+
+        Grid.Column<Invoice> paymentTimeColumn =
+        grid.addColumn(Invoice::getPaymentTime).setAutoWidth(true);
+
+        Grid.Column<Invoice> isPaidColumn =
+                grid.addColumn(Invoice::isPaid).setAutoWidth(true);
+
+        Grid.Column<Invoice> invoiceTypeColumn =
+                grid.addColumn(Invoice::getType).setAutoWidth(true);
+
+        Grid.Column<Invoice> paymentMethodColumn =
+                grid.addColumn(Invoice::getPaymentMethod).setAutoWidth(true);
+
+
+
+
         grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
 
-        // when a row is selected or deselected, populate form
-        grid.asSingleSelect().addValueChangeListener(event -> {
-            if (event.getValue() != null) {
-                UI.getCurrent().navigate(String.format(INVOICE_EDIT_ROUTE_TEMPLATE, event.getValue().getId()));
-            } else {
-                clearForm();
-                UI.getCurrent().navigate(InvoicesView.class);
-            }
-        });
+        List<Invoice> invoiceList = invoiceService.invoiceList();
+        GridListDataView<Invoice> dataView = grid.setItems(invoiceList);
+        InvoicesFilter invoicesFilter = new InvoicesFilter(dataView);
 
-        // Configure Form
-        binder = new BeanValidationBinder<>(Invoice.class);
+        grid.getHeaderRows().clear();
+        HeaderRow headerRow = grid.appendHeaderRow();
 
-        // Bind fields. This is where you'd define e.g. validation rules
+        headerRow.getCell(invoiceNumberColumn).setComponent(
+                createFilterHeader("Invoice Number", invoicesFilter::setInvoiceNumber));
+        headerRow.getCell(clientFullNameColumn).setComponent(
+                createFilterHeader("Client", invoicesFilter::setClientFullName));
+        headerRow.getCell(contractNumberColumn).setComponent(
+                createFilterHeader("Contract Number", invoicesFilter::setContractNumber));
+        headerRow.getCell(amountColumn).setText("Amount");
+        headerRow.getCell(issueDateColumn).setText("Issue Date");
+        headerRow.getCell(paymentTimeColumn).setText("Payment Time");
+        headerRow.getCell(isPaidColumn).setText("Paid");
+        headerRow.getCell(invoiceTypeColumn).setText("Type");
+        headerRow.getCell(paymentMethodColumn).setText("Payment Method");
 
-        binder.bindInstanceFields(this);
 
-        cancel.addClickListener(e -> {
-            clearForm();
-            refreshGrid();
-        });
-
-        save.addClickListener(e -> {
-            try {
-                if (this.invoice == null) {
-                    this.invoice = new Invoice();
-                }
-                binder.writeBean(this.invoice);
-                invoiceService.update(this.invoice);
-                clearForm();
-                refreshGrid();
-                Notification.show("Data updated");
-                UI.getCurrent().navigate(InvoicesView.class);
-            } catch (ObjectOptimisticLockingFailureException exception) {
-                Notification n = Notification.show(
-                        "Error updating the data. Somebody else has updated the record while you were making changes.");
-                n.setPosition(Position.MIDDLE);
-                n.addThemeVariants(NotificationVariant.LUMO_ERROR);
-            } catch (ValidationException validationException) {
-                Notification.show("Failed to update the data. Check again that all values are valid");
-            }
-        });
+        add(grid);
     }
 
-    @Override
-    public void beforeEnter(BeforeEnterEvent event) {
-        Optional<Long> invoiceId = event.getRouteParameters().get(INVOICE_ID).map(Long::parseLong);
-        if (invoiceId.isPresent()) {
-            Optional<Invoice> invoiceFromBackend = invoiceService.get(invoiceId.get());
-            if (invoiceFromBackend.isPresent()) {
-                populateForm(invoiceFromBackend.get());
-            } else {
-                Notification.show(String.format("The requested invoice was not found, ID = %s", invoiceId.get()), 3000,
-                        Notification.Position.BOTTOM_START);
-                // when a row is selected but the data is no longer available,
-                // refresh grid
-                refreshGrid();
-                event.forwardTo(InvoicesView.class);
-            }
+    private static Component createFilterHeader(String labelText,
+                                                Consumer<String> filterChangeConsumer) {
+        return getComponent(labelText, filterChangeConsumer);
+    }
+
+    private static class InvoicesFilter {
+        private final GridListDataView<Invoice> dataView;
+
+        private String invoiceNumber;
+        private String clientFullName;
+
+        private String contractNumber;
+
+
+        public InvoicesFilter(GridListDataView<Invoice> dataView) {
+            this.dataView = dataView;
+            this.dataView.addFilter(this::test);
+        }
+
+        public void setInvoiceNumber(String invoiceNumber) {
+            this.invoiceNumber = invoiceNumber;
+            this.dataView.refreshAll();
+        }
+
+        public void setClientFullName(String clientFullName) {
+            this.clientFullName = clientFullName;
+            this.dataView.refreshAll();
+        }
+
+        public void setContractNumber(String contractNumber){
+            this.contractNumber = contractNumber;
+            this.dataView.refreshAll();
+        }
+
+
+        public boolean test(Invoice invoice) {
+            boolean matchesInvoiceNumber = matches(invoice.getNumber(), invoiceNumber);
+            boolean matchesClientFullName = matches(invoice.getClient().getFullName(), clientFullName);
+            boolean matchesContractNumber = matches(invoice.getContract().getNumber(), contractNumber);
+            return matchesInvoiceNumber && matchesClientFullName && matchesContractNumber;
+        }
+
+        private boolean matches(String value, String searchTerm) {
+            return searchTerm == null || searchTerm.isEmpty()
+                    || value.toLowerCase().contains(searchTerm.toLowerCase());
         }
     }
 
-    private void createEditorLayout(SplitLayout splitLayout) {
-        Div editorLayoutDiv = new Div();
-        editorLayoutDiv.setClassName("editor-layout");
-
-        Div editorDiv = new Div();
-        editorDiv.setClassName("editor");
-        editorLayoutDiv.add(editorDiv);
-
-        FormLayout formLayout = new FormLayout();
-        number = new TextField("Number");
-        amount = new TextField("Amount");
-        contractNumber = new TextField("Contract Number");
-        client = new TextField("Client");
-        issueDate = new TextField("Issue Date");
-        paymentTime = new TextField("Payment Time");
-        type = new TextField("Type");
-        paid = new Checkbox("Paid");
-        paymentMethod = new TextField("Payment Method");
-        formLayout.add(number, amount, contractNumber, client, issueDate, paymentTime, type, paid, paymentMethod);
-
-        editorDiv.add(formLayout);
-        createButtonLayout(editorLayoutDiv);
-
-        splitLayout.addToSecondary(editorLayoutDiv);
-    }
-
-    private void createButtonLayout(Div editorLayoutDiv) {
-        HorizontalLayout buttonLayout = new HorizontalLayout();
-        buttonLayout.setClassName("button-layout");
-        cancel.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        buttonLayout.add(save, cancel);
-        editorLayoutDiv.add(buttonLayout);
-    }
-
-    private void createGridLayout(SplitLayout splitLayout) {
-        Div wrapper = new Div();
-        wrapper.setClassName("grid-wrapper");
-        splitLayout.addToPrimary(wrapper);
-        wrapper.add(grid);
-    }
-
-    private void refreshGrid() {
-        grid.select(null);
-        grid.getDataProvider().refreshAll();
-    }
-
-    private void clearForm() {
-        populateForm(null);
-    }
-
-    private void populateForm(Invoice value) {
-        this.invoice = value;
-        binder.readBean(this.invoice);
-
-    }
 }
